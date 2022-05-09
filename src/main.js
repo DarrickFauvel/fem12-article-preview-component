dcb8a6d35498ce79fb0ae3ad624300afde0f941b
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-05-09 19:45:44 -0400

    Add toggle method

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-import './css/main.css'
+import './assets/styles/main.css'
+
+const shareBtn = document.querySelector('.share__btn')
+
+const toggleActive = (e) => {
+  e.preventDefault()
+  const card = document.querySelector('.card')
+  card.classList.toggle('active')
+}
+
+shareBtn.addEventListener('click', toggleActive)
